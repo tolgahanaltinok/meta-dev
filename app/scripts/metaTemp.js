5cd1bff8393539beaa612c340aaf2170b5bffdec
tolgahanaltinok <tolgahanaltinok@gmail.com>
2014-08-15 17:15:05 +0300

    first

---
+++ app/scripts/metaTemp.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('metaTemp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: "/",
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise("/");
+  })
+;
