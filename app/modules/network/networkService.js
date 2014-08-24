b857e68ba6f5cca9a03cc81377c27cfb5378341a
tolgahanaltinok <tolgahanaltinok@gmail.com>
2014-08-24 22:55:51 +0300

    Create networkService.js

---
+++ app/modules/network/networkService.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('metaTemp')
+  .factory('authService',
+    ['$http','authServiceHelper','$cookieStore',
+        function ($http, authServiceHelper, $cookieStore) {
+}]);
