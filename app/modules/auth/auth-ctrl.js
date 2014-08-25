722711fd9072dfb74509440f51becfa6818299f9
tolgahanaltinok <tolgahanaltinok@gmail.com>
2014-08-25 19:27:32 +0300

    Commit message

---
+++ app/modules/auth/auth-ctrl.js
@@ -0,0 +1,43 @@
+﻿app.controller('loginCtrl', ['$scope', '$state', 'authService','$http', function ($scope, $state, authService,$http) {
+    $scope.login = function (userLogin) {
+        $scope.errorMessage = '';
+        authService.login(userLogin).$promise
+        .then(function (data) {
+            $state.go('main');
+        }).catch(function (errorResponse) {
+            if (errorResponse.status == 404) {
+                $scope.errorMessage = errorResponse.data;
+            }
+            if (errorResponse.status === 400) {
+                $scope.errorMessage = "Invalid Email/Password";
+            }
+            else {
+                $scope.errorMessage = "An error occured while performing this action. Please try after some time.";
+            }
+        });
+    };
+}])
+.controller('registerCtrl', ['$scope', '$state', 'authService', function ($scope, $state, authService) {
+    $scope.register = function (userRegistration) {
+        if (userRegistration.Password !== userRegistration.ConfirmPassword) {
+            console.log("Passwords do not match");
+            return;
+        }
+
+        $scope.errorMessage = '';
+
+        authService.registerUser(userRegistration).$promise
+        .then(function (data) {
+            return authService.login({ userName: userRegistration.Username, password: userRegistration.Password }).$promise.then(function (data) {
+                $state.go('main');
+            });
+        }).catch(function (error) {
+            if (error.status === 400) {
+                $scope.errorMessage = "Email already exists.";
+            }
+            else {
+                $scope.errorMessage = "An error occured while performing this action. Please try after some time.";
+            }
+        });
+    };
+}])
